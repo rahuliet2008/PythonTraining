0462f4dc974696917e87733cb463779e0ed34b92
Rahul Singh <rahul.singh@ing.com>
2024-04-08 15:05:22 +0200

    programs

---
+++ conditional_program/program.py
@@ -0,0 +1,14 @@
+
+x=int(input("Please enter value of X : "))
+y= int(input("Please enter value of y : "))
+
+if(x/y==5):
+    x=100
+    print("value of X: ", x)   
+if(x<y):
+    x+=x
+    print("value of X: ", x)   
+if(x>y):
+    x+=1
+    print("value of X: ", x)   
+         
---
+++ temperature/temp_converter.py
@@ -0,0 +1,5 @@
+celcius_temp=input("Please enter temperature in celcius : ")
+
+fah_temp=(1.8 * float(celcius_temp)) + 32
+
+print("Temperature in Fahrenheit : ",fah_temp)
